a392501d68ba6a84b40919f163a12f1727adb0f4
Steve Freakin' Perry <perryst5@gmail.com>
2022-02-21 19:33:01 -0500

    Remove helloworld. Add first base components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
+  <Header />
+  <IntroContent msg="Test Message" />
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<style>
+<style lang="scss">
+body {
+  margin: 0;
+  h1 {
+    color: red;
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  padding-top: 60px;
+  background: #333;
+  height: 100vh;
+  width: 100vh;
 }
 </style>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Header from './components/Header.vue'
+import IntroContent from './components/IntroContent.vue'
+</script>
--- src/components/IntroContent.vue
+++ src/components/IntroContent.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
 
 <template>
   <h1>{{ msg }}</h1>
@@ -26,7 +19,6 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -50,3 +42,14 @@ code {
   color: #304455;
 }
 </style>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "IntroContent",
+  props: {
+    msg: String
+  }
+});
+</script>
