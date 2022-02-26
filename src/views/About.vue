a12f97ee3d32b39c8b0d8be6b6ef961238249573
Steve Freakin' Perry <perryst5@gmail.com>
2022-02-26 15:44:34 -0500

    Add empty components for deployment

---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+    <Header />
+  </div>
+</template>
+
+<style lang="scss">
+@import 'normalize.css';
+@import url('https://fonts.googleapis.com/css2?family=Audiowide&display=swap');
+
+#app {
+  font-family: Audiowide, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  background: #111;
+  height: 100vh;
+}
+</style>
+
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+</script>
