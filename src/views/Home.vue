da70924c9b16c58fe4b0ac886a01ff349edd4e87
Steve Perry <perryst5@gmail.com>
2025-05-10 20:10:19 -0400

    try it again

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,9 +10,9 @@
         <p>I've worked with Git extensively throughout my career, using GitHub, GitLab, and BitBucket for repository hosting. I've set up continuous integration and deployment pipelines to streamline our development process via GitHub Actions.</p>
         <p>For frontend build technologies, I've used the obvious languages: HTML, CSS (Sass mostly), and Javascript. I'm versed in OOP principles due to the use of PHP within HLM's proprietary CMS, <a href="https://www.highlevelmarketing.com/technology.html">MYCE</a>.</p>
         <p>My role has shifted throughout the years to a more full-stack one, as I contribute more frequently to HLM's internal app platform.</p>
-        <p>One of my recent projects involved using VueJS with WebPack as a bundler and BootStrap for our CSS framework. Working with other Senior Developers, we rewrote our RESTful API for our new enterprise-level application. It leveraged Amazon's API Gateway (and other associated AWS services).</p>
+        <p>One of my recent projects involved using VueJS with WebPack as a bundler and Bootstrap for our CSS framework. Working with other Senior Developers, we rewrote our RESTful API for our new enterprise-level application. It leveraged Amazon's API Gateway (and other associated AWS services).</p>
         <p>The next iteration of HLM's reporting dashboard is being built with .NET, showcasing our commitment to leveraging technologies for enhanced performance and scalability.</p>
-        <p>The website you're currently on is a single page application built in VueJS, using Vite as the bundler and Netlify as a hosting service. Here's <a href="https://github.com/perryst5/portfolio-2022">the code</a>, if you're interested.</p>
+        <p>The website you're currently viewing is a single page application built in VueJS, using Vite as the bundler and Netlify as a hosting service. Here's <a href="https://github.com/perryst5/portfolio-2022">the code</a>, if you're interested.</p>
         <p>Please take a look at my <a href="https://linkedin.com/in/steeeveperry">LinkedIn profile</a> for more information about my work history, or download my <a href="/documents/sperry_resume.pdf">resume</a>.</p>
       </div>
       
